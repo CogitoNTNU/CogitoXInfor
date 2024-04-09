7dc9f047bf83033f082c4dcc28ade2eab98a11f8
Jens Martin Norheim Berget <jmberget@stud.ntnu.no>
2024-04-09 20:18:05 +0200

    feat: :sparkles: Finished script cal_recommendation.py that correctly enters embeddings into Recommendations-database

--- backend/cal_recommendation.py
+++ backend/cal_recommendation.py
@@ -1,80 +1,83 @@
 import os
 import django
 
+print("Test", flush=True)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cogitoXInfor.settings')
 django.setup()
+
+print("Test2", flush=True)
 
 import csv
 from GoogleAmazone.models import Recommendations, Products
 from sentence_transformers import SentenceTransformer, util
 
+Recommendations.objects.all().delete()
+print("Test3", flush=True)
+
 # Add a recommendation calculation here to precompile recommendations 
-# and add then to the database in the Recommendations table
+# and add them to the database in the Recommendations table
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
+print("Test4", flush=True)
+
 all_products = Products.objects.all()
+print("All products = ", all_products, flush=True)
 product_titles = [product.title for product in all_products]
 product_descriptions = [product.description for product in all_products]
 
 # Matching product id's with product titles
-id_title = {}
-id_desc = {}
+id_title_embed = {}
+id_desc_embed = {}
+i = 0
+print("All products length = ", len(all_products), flush=True)
 for product in all_products:
-    id_title[product.id] = model.encode(product.title)
-    id_desc[product.id] = model.encode(product.description)
+    i += 1
+    print(i, flush=True)
+    id_title_embed[product.id] = model.encode(product.title)
+    id_desc_embed[product.id] = model.encode(product.description)
     
-#Vi kom så langt! jeg la til dette for å teste  
+print("Begynner å embedde", flush=True)
+# print("id_title_embed.items() = ", id_title_embed.items(), flush=True)
+# print("id_desc_embed.items() = ", id_desc_embed.items(), flush=True)
 
+# Compute the cosine similarity between the title and description of all pairs of products and store the results in dictionaries
+cos_sim_title = {}
+for id1, title1 in list(id_title_embed.items()):
+    for id2, title2 in list(id_title_embed.items()):
+        if id1 != id2:
+            if (id1, id2) not in cos_sim_title and (id2, id1) not in cos_sim_title:
+                cos_sim_title[(id1, id2)] = util.cos_sim(title1, title2)
 
-# print("id_desc = ", id_desc, flush=True)
-    
-# EMBEDDING ============================================================================================================
+cos_sim_desc = {}
+for id1, desc1 in list(id_desc_embed.items()):
+    for id2, desc2 in list(id_desc_embed.items()):
+        if id1 != id2:
+            if (id1, id2) not in cos_sim_desc and (id2, id1) not in cos_sim_desc:
+                cos_sim_desc[(id1, id2)] = util.cos_sim(desc1, desc2)
 
+combined_dict = {}
+
+for key, value in cos_sim_title.items():
+    if key in cos_sim_desc:
+        combined_dict[key] = (value + cos_sim_desc[key]) / 2
+
+for pair, sim_title in list(combined_dict.items()):
+    sim_combined = combined_dict[pair]
+
+    recommendation = Recommendations(col=str(pair[0]), row=str(pair[1]), value=sim_combined)
+    recommendation.save()
+
+print("Recommendations saved to the database", flush=True)
+print("Cosine similarities titles = ", cos_sim_title, flush=True)
 print("", flush=True)
-#Encoding titles of all other products and using cosine similarity (vectors)
-title_encoding = {}
-emb1 = model.encode(product.title)
-for title in product_titles:
-    emb2 = model.encode(title)
-    cos_sim = util.cos_sim(emb1, emb2)
-    title_encoding[title] = cos_sim
-print("Title encoding = ", title_encoding, flush=True)
-
-# Encoding descriptions of all other products and using cosine similarity (vectors)
-desc_encoding = {}
-emb3 = model.encode(product.description)
-for desc in product_descriptions:
-    emb4 = model.encode(desc)
-    cos_sim = util.cos_sim(emb3, emb4)
-    desc_encoding[desc] = cos_sim
-# print("Product descriptions = ", product_descriptions, flush=True)
-print("Description encoding = ", desc_encoding, flush=True)
+print("Cosine similarities descriptions = ", cos_sim_desc, flush=True)
 print("", flush=True)
 
-# Combining and taking the average of the two encodings
-combined_encoding = {}
-for title, cos_sim in title_encoding.items():
-    id = [id for id, title2 in id_title.items() if title == title2][0]
-    print("Current product ID = ", id, flush=True)
-    desc = id_desc[id]
-    print("Description of current product = ", desc, flush=True)
-    combined_encoding[title] = (cos_sim + desc_encoding[desc]) / 2
-print("",flush=True)
-print("Combined encoding = ", combined_encoding, flush=True)
+
+print("Combined dictionary = ", combined_dict, flush=True)
 
 
-# Sorting the combined encodings
-sorted_combined = sorted(combined_encoding.items(), key=lambda item: item[1], reverse=True)
 
-# Skip the first one (the product itself) and select the next top products
-top_products = [title for title, cos_sim in sorted_combined[1:amount]]
 
-# Get the top product IDs from the top products
-top_product_ids = [
-    id for id, title in id_title.items() if title in top_products
-]
-
-# Get the actual top products from the IDs
-top_products = Products.objects.filter(id__in=top_product_ids)
-print("Top products = ", top_products, flush=True)  
